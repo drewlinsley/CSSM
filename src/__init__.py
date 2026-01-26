d7efa5c138ccc454bba73953f48a66c9741348e7
Drew Linsley <drewlinsley@gmail.com>
2026-01-26 14:41:47 -0500

    Make data imports optional in src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,19 @@
 """CSSM: Cepstral State Space Models for vision tasks."""
 
-from .data import get_imagenette_video_loader
 from .models import GatedCSSM, HGRUBilinearCSSM, TransformerCSSM
 
+# Data loaders are optional (may have extra dependencies)
+try:
+    from .data import get_imagenette_video_loader
+    _HAS_DATA = True
+except ImportError:
+    _HAS_DATA = False
+
 __all__ = [
-    "get_imagenette_video_loader",
     "GatedCSSM",
     "HGRUBilinearCSSM",
     "TransformerCSSM",
 ]
+
+if _HAS_DATA:
+    __all__.append("get_imagenette_video_loader")
